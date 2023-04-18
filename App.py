7c33dc0fd946c229210e1c4e1d82d6f63a41fa17
Bhargav Chowdary <pusuluribhargavuk@gmail.com>
2023-04-18 11:43:46 +0100

    sprint 2

--- App.py
+++ App.py
@@ -3,13 +3,38 @@ from flask import Flask
 
 from datetime import datetime
 import os
+import xml.etree.ElementTree as ET
+import requests
+from xml.etree import ElementTree as etree
 
 app = Flask(__name__,  template_folder='templates', static_folder='static')
 
 
-@app.route('/', methods=['GET', 'POST'])
-def signomein():
-    return render_template("home.html")
+@app.route('/livecyclehireupdates', methods=['GET', 'POST'])
+def livecyclehireupdates():
+    document = requests.get("https://tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml")
+    tree = etree.fromstring(document.content)
+    stationArr = []
+    for station in tree.findall('station'):
+        response = {}
+        response['id'] = station.find('id').text
+        response['name'] = station.find('name').text
+        response['terminalName'] = station.find('terminalName').text
+        response['lat'] = station.find('lat').text
+        response['long'] = station.find('long').text
+        response['installed'] = station.find('installed').text
+        response['removalDate'] = station.find('removalDate').text
+        response['locked'] = station.find('locked').text
+        response['installDate'] = station.find('installDate').text
+        response['temporary'] = station.find('temporary').text
+        response['nbBikes'] = station.find('nbBikes').text
+        response['nbStandardBikes'] = station.find('nbStandardBikes').text
+        response['nbEBikes'] = station.find('nbEBikes').text
+        response['nbEmptyDocks'] = station.find('nbEmptyDocks').text
+        response['nbDocks'] = station.find('nbDocks').text
+        stationArr.append(response)
+    return stationArr
+
 
 
 
